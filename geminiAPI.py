ab50fbc69375ce28784fdf3845d5e1927db90f0e
tesu-dice <terariadaiki@gmail.com>
2025-03-26 12:31:57 +0900

    とりあえず動いた

--- geminiAPI.py
+++ geminiAPI.py
@@ -16,7 +16,17 @@ import talk_VoiceVoxEngine as talkVoice
 #APIkeyの設定
 f = open("myAPI.txt", "r")
 yourAPIkey = f.readline()
+f.close()
 genai.configure(api_key=yourAPIkey)
+
+#会話設定
+f= open("Character_setting.txt", encoding="utf-8")
+setting=""
+for line in f:
+    line.strip("\n")
+    setting +=line
+# print(setting)
+# input()
 
 #モデルの種類確認
 for m in genai.list_models():
@@ -24,10 +34,43 @@ for m in genai.list_models():
         print(m.name)
 
 # モデルを準備
-model = genai.GenerativeModel('gemini-2.0-flash')
+generation_config = {"temperature":1,
+                     "top_p":0.95,
+                     "top_k":40,
+                     "response_mime_type":"text/plain"
+                     }#, "max_output tokens=500":500}
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    }
+  ]
+model = genai.GenerativeModel(model_name = 'gemini-2.0-pro-exp',
+                              generation_config= generation_config,
+                              safety_settings=safety_settings)
 
-task = " あなたは、コンピュータ上のサポートAIです。以下の会話に対しての返答を行ってください。また、その際、電話で話すような形でのみ返答し、文章量は3行程度にしてください。"
+chat_session = model.start_chat(history=[
+    {"role":"user","parts":[setting]},
+    {"role":"model","parts":["了解しました。"]}
+    ])
 
-response = model.generate_content(task+"こんにちは、あなたにはどんなことができるのですか。")
-print(response.text)
-talkVoice.text_to_speech(response.text)
+
+while 1:
+    input_text = input(":")
+    response = chat_session.send_message(input_text)
+    print(response.text)
+    talkVoice.text_to_speech(response.text)
+    print("history")
+    print(chat_session.history)
