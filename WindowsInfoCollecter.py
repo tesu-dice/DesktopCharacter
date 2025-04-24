c70a4032495b2f791f23cdfff84026f2cea80f60
tesu-dice <terariadaiki@gmail.com>
2025-04-25 01:10:19 +0900

    ウィンドウ名の取得と 5 分毎の会話を実装

---
+++ WindowsInfoCollecter.py
@@ -0,0 +1,51 @@
+"""
+Windowsからデータを取得して受け渡す用のクラス
+
+"""
+import datetime
+import win32gui
+
+
+class win_info_collector():
+    def __init__(self):
+        print("win_info_collecter init() called")
+        self.time_init = datetime.datetime.now()
+        self.time_last = datetime.datetime.now()
+        self.get_datetime()
+        self.get_nowday()
+
+    #作業中のウィンドウのIDゲットしてそのタイトルを文字列で返す。
+    def get_activate_window(self):
+        window_title = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+        return window_title
+    
+    #YYYY-MM-DD hh:mm:ss.ssssを文字列で返す。
+    def get_datetime(self):
+        nowtime = str(datetime.datetime.now())
+        print("win_info_collecter get_nowtime() nowtime = " + nowtime )
+        return nowtime
+
+    #今日の日付を取得
+    def get_nowday(self):
+        day = datetime.datetime.now
+        day = datetime.date.today()
+        print("win_info_collector get_nowday() day = " + str(day) ) 
+        return day
+    
+    def check_freetime(self):
+        freetime =  datetime.datetime.now() - self.time_last
+        freetime = freetime.seconds
+        print(freetime)
+        print(type(freetime))
+        if freetime >= 300:
+            self.lasttime = datetime.datetime.now()
+            return True
+        else:
+            return False
+        
+
+
+
+
+if __name__ == "__main__":
+    this = win_info_collector()
--- main.py
+++ main.py
@@ -2,16 +2,22 @@
 いろいろなプログラムの中継訳
 
 """
-
+#ライブラリ
 import tkinter as tk
 import subprocess
 
+#プログラム
 import UI
 import geminiAPI
+import WindowsInfoCollecter
+
 class myapp():
     def __init__(self):
+        #初期化
         self.ui = UI.ui(self)
         self.ai = geminiAPI.geminiAI()
+        self.WinInfo = WindowsInfoCollecter.win_info_collector()
+
         #voicevoxを起動
         try:
             result = subprocess.Popen("windows-nvidia/run.exe")
@@ -19,12 +25,20 @@ class myapp():
             print("VoiceVoxEngineの実行に失敗しました。")
             print(e)
 
-    #UIで取得したテキストをAIに伝える
-    def SendMessage_UItoAI(self, text):
-        anser = self.ai.response(text)
+    #UIからの入力テキストをAIに伝え、UIにログを追加
+    def SendMessage_toAI(self, text):
+        t = "現在時刻：" + self.WinInfo.get_datetime() + "\n"
+        w = "作業中窓：" + self.WinInfo.get_activate_window() + "\n"
+        self.ui.add_log(t + w + text)
+        anser = self.ai.response(t + w + text)
         self.ui.add_log(anser)
 
-
+    #状態監視の実行
+    def update(self):
+        print("main.py update() called")
+        if self.WinInfo.check_freetime():
+            self.SendMessage_toAI("System:ユーザは以下のように作業中です。話しかけてください。")
+        self.ui.win.after(5000, self.update)
 
 
 
@@ -38,4 +52,6 @@ class myapp():
 print("main.py end")
 if __name__ =="__main__":
     app = myapp()
+    app.ui.win.after(500, app.update)
+
     app.ui.win.mainloop()
